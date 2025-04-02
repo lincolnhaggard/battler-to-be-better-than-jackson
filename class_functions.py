17dc3ed810108cb240a649442497f399572e3883
lincolnhaggard <lincoln.haggard@ucas-edu.net>
2025-04-02 18:17:54 +0000

    added more classes

--- class_functions.py
+++ class_functions.py
@@ -1,8 +1,24 @@
 import math
+import random
 
-def invis(self,value):
+def invis(self,value,other):
     self.dodge=math.floor(80-(80*(0.916^math.floor(value))))
     self.d_timer=2
 
-def block(self,value):
+def block(self,value,other):
     self.dmg_reduce=value
+
+def true_strike(self,value,other):
+    self.dmg=value
+    self.unblockable=True
+
+def stun_strike(self,value,other):
+    self.dmg=value
+    other.stuned=True
+
+def meditate(self,value,other):
+    self.bonus=value
+
+def leaf_storm(self,value,other):
+    self.dmg=value
+    self.multi_hit=random.randint(3,10)
--- class_stats.py
+++ class_stats.py
@@ -1,15 +1,25 @@
 from class_functions import *
 
 #stats add to 10
-CLASS_STATS={   "Mage":{"health":0.5,"attack":0.5,"magic":4,"speed":3,'special':2,
-                             #format unlocklvl:(name,type,value,magic cost)
-                             #if move is spc lvl:(name,'spc',value,magic cost,function)
-                        "moves":{0:("fire bolt",'dmg',14,5),
-                              0:('heal','hl',5,15),
-                              3:("fire ball","dmg",40,60),
-                              5:('invisibilty',"spc",5,40,invis)}},
+CLASS_STATS={   "Mage":{"health":2,"attack":1,"magic":4,"speed":3,
+                             #format unlocklvl:(name,type,value,magic cost,multi)
+                             #if move is spc lvl:(name,'spc',value,magic cost,multi,function)
+                        "moves":{0:("fire bolt",'dmg',10,5,"mgc"),
+                              0:('heal','hl',5,15,"mgc"),
+                              3:("fire ball","dmg",25,60,"mgc"),
+                              5:('invisibilty',"spc",5,40,"mgc",invis)}},
 
-                "Fighter":{"heath":2.5,"attack":2.5,"magic":0.5,"speed":2.5,"special":2,
-                           'moves':{0:('slash',"dmg",5,None),
-                                    0:("block","spc",5,None,block),
-                                    3:('swift strike','spc',10,8,)}}}
+                "Fighter":{"heath":3.5,"attack":3.5,"magic":0.5,"speed":2.5,
+                           'moves':{0:('slash',"dmg",5,None,"atk"),
+                                    0:("block","spc",5,None,"spd",block),
+                                    3:('true strike','spc',10,8,"dmg",true_strike),
+                                    5:("stun strike","spc",15,10,"dmg",stun_strike)}},
+                                    
+                  "Druid":{"health":3.5,"attack":1,"magic":3,"speed":2.5,
+                           "moves":{0:("leaf","dmg",5,5,"mgc"),
+                                    0:("heal","hl",15,15,"mgc"),
+                                    3:("meditate","spc",10,5,"mgc",meditate),
+                                    5:("leaf storm","spc",5,30,"mgc",leaf_storm)}},
+                                    
+                  "Cleric":{"health":6,"attack":0.5,"magic":3,"speed":0.5,
+                            "moves":{0:("holy fire","dmg")}}                  }
